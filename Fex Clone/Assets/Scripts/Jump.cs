5465ff8a4e2e7ed8b32aaf336f1ab39e32a79f97
RulaZSmooth <zimbakob@myumanitoba.ca>
2017-12-06 16:59:01 -0600

    Sync

---
+++ Fex Clone/Assets/Scripts/Jump.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+
+	public float jumpForce;
+	public bool grounded;
+	private Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space") && grounded == true) {
+			rigidBody.AddForce (Vector3.up * jumpForce, ForceMode2D.Impulse);
+		}
+	}
+
+	void onCollisionStay(Collision2D collision){
+		if (collision.gameObject.tag == "Ground") {
+			grounded = true;
+		}
+	}
+
+	void onCollisionExit(Collision2D collision){
+		if(collision.gameObject.tag == "Ground"){
+			grounded = false;
+		}
+	}
+}
---
+++ Fex Clone/Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float speed;
+	// Use this for initialization
+	// I will leave it blank for now because we won't need it unitl we actually have a start in game
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	//interwebs told me to use FixedUpdate for working with physics vs Update but we can prolly move it back
+	/*
+	void Update () {
+		
+	}
+	*/
+	void FixedUpdate(){
+		if (Input.GetKey ("left")) {
+			gameObject.transform.Translate (Vector3.left * speed * Time.deltaTime);
+		}
+		if (Input.GetKey ("right")) {
+			gameObject.transform.Translate (Vector3.right * speed * Time.deltaTime);
+		}
+	}
+
+}
