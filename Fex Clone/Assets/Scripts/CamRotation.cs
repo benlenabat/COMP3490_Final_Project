1f12f033f34473052a032cc70a4a76d615e9ba0e
benlenabat <benlenabat@gmail.com>
2017-12-10 16:08:30 -0600

    control change
    changed gontrols so they move the play properly when the view is changed relative to that person, for some reason they are buggy I cannot figure out why

--- Fex Clone/Assets/Scripts/CamRotation.cs
+++ Fex Clone/Assets/Scripts/CamRotation.cs
@@ -3,18 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CamRotation : MonoBehaviour {
-	
-	// Update is called once per frame
 
-	void Update () {
+    // Update is called once per frame
+
+    void Update () { 
 
 		if (Input.GetKeyUp ("d")) {
-			transform.RotateAround(Vector3.zero, Vector3.up, 90);
-		}
+			transform.RotateAround(Vector3.zero, Vector3.up, -90);
+        }
 
 		if (Input.GetKeyUp ("a")) {
-			transform.RotateAround(Vector3.zero, Vector3.up, -90);
-
+			transform.RotateAround(Vector3.zero, Vector3.up, 90);
         }
 
 		//transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
--- Fex Clone/Assets/Scripts/Movement.cs
+++ Fex Clone/Assets/Scripts/Movement.cs
@@ -5,11 +5,41 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
 
 	public float speed;
-	// Use this for initialization
-	// I will leave it blank for now because we won't need it unitl we actually have a start in game
+    // Use this for initialization
+    // I will leave it blank for now because we won't need it unitl we actually have a start in game
+    public Vector3 leftK; //variable to holdm direction change for left arrow key
+    public Vector3 rightK; //variable to hold direction change for right arrow key
+    public string view;
+
 	void Start () {
-		
+        leftK = Vector3.left;
+        rightK = Vector3.right;
+        view = "front";
 	}
+
+    public void frontSide() //when camera changes to the default front view
+    {
+        leftK = Vector3.left;
+        rightK = Vector3.right;
+    }
+
+    public void backSide() //when camera changes to back view
+    {
+        leftK = Vector3.right;
+        rightK = Vector3.left;
+    }
+
+    public void leftSide() //when camera changes to left view
+    {
+        leftK = Vector3.forward;
+        rightK = Vector3.back;
+    }
+
+    public void rightSide() //when camera changes to right view
+    {
+        leftK = Vector3.back;
+        rightK = Vector3.forward;
+    }
 	
 	// Update is called once per frame
 	//interwebs told me to use FixedUpdate for working with physics vs Update but we can prolly move it back
@@ -20,11 +50,57 @@ public class Movement : MonoBehaviour {
 	*/
 	void FixedUpdate(){
 		if (Input.GetKey ("left")) {
-			gameObject.transform.Translate (Vector3.left * speed * Time.deltaTime);
+			gameObject.transform.Translate (leftK * speed * Time.deltaTime);
 		}
 		if (Input.GetKey ("right")) {
-			gameObject.transform.Translate (Vector3.right * speed * Time.deltaTime);
+			gameObject.transform.Translate (rightK * speed * Time.deltaTime);
 		}
-	}
+        if (Input.GetKeyUp("d"))
+        {
+            if (view == "front")
+            {
+                view = "right";
+                rightSide();
+            }
+            else if(view == "right")
+            {
+                view = "back";
+                backSide();
+            }
+            else if(view == "back")
+            {
+                view = "left";
+                leftSide();
+            }
+            else if(view == "left")
+            {
+                view = "front";
+                frontSide();
+            }
+        }
+        else if (Input.GetKeyUp("a"))
+        {
+            if (view == "front")
+            {
+                view = "left";
+                leftSide();
+            }
+            else if (view == "left")
+            {
+                view = "back";
+                backSide();
+            }
+            else if (view == "back")
+            {
+                view = "right";
+                rightSide();
+            }
+            else if (view == "right")
+            {
+                view = "front";
+                frontSide();
+            }
+        }
+    }
 
 }
