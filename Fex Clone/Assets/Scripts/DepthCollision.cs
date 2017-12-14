e29e1aa07f4911fefdb850aa617a668ff5c4e21f
benlenabat <benlenabat@gmail.com>
2017-12-13 20:52:26 -0600

    rough changes for background objects
    -needs work will do later

--- Fex Clone/Assets/Scripts/DepthCollision.cs
+++ Fex Clone/Assets/Scripts/DepthCollision.cs
@@ -94,31 +94,52 @@ public class DepthCollision : MonoBehaviour {
         {
             GameObject ground = groundObjects[i]; //setting ground object to current platform
             BoxCollider collider = ground.GetComponentInChildren<BoxCollider>(); //new collider object
-            collider.center = Vector3.zero; //default position
-
-            Vector3 colliderPos = collider.transform.TransformPoint(collider.center); //colliderPos centered on collider
-
-            Vector3 playerPos = playerTransform.position; //new vector3 on player
-
-            Vector3 newColliderPos;
-
-            //move platform collider depending on what side the camera is facing 
-            GameObject player = GameObject.FindGameObjectWithTag("Player"); //gets player
-            float rotation = Mathf.Rad2Deg * player.transform.rotation.y; //get rotation to determine what angle we are looking at
-            if (Mathf.Abs(Mathf.Round(rotation)) == 41) //if looking from left or right
+            if (collider != null)
             {
-                newColliderPos = new Vector3(playerPos.x, colliderPos.y, colliderPos.z);
-                prizeCheck(player, true);
+                collider.center = Vector3.zero; //default position
+                Vector3 colliderPos = collider.transform.TransformPoint(collider.center); //colliderPos centered on collider
+                Vector3 playerPos = playerTransform.position; //new vector3 on player
+                Vector3 newColliderPos;
+
+                //move platform collider depending on what side the camera is facing 
+                GameObject player = GameObject.FindGameObjectWithTag("Player"); //gets player
+                float rotation = Mathf.Rad2Deg * player.transform.rotation.y; //get rotation to determine what angle we are looking at
+                if (Mathf.Abs(Mathf.Round(rotation)) == 41) //if looking from left or right
+                {
+                    newColliderPos = new Vector3(playerPos.x, colliderPos.y, colliderPos.z);
+                    prizeCheck(player, true);
+                }
+                else //if looking from front or back
+                {
+                    newColliderPos = new Vector3(colliderPos.x, colliderPos.y, playerPos.z);
+                    prizeCheck(player, false);
+                }
+
+                newColliderPos = collider.transform.InverseTransformPoint(newColliderPos); //local space
+
+                collider.center = newColliderPos; //set in world
             }
-            else //if looking from front or back
+            else
             {
-                newColliderPos = new Vector3(colliderPos.x, colliderPos.y, playerPos.z);
-                prizeCheck(player, false);
+                GameObject player = GameObject.FindGameObjectWithTag("Player"); //gets player
+                float rotation = Mathf.Rad2Deg * player.transform.rotation.y; //get rotation to determine what angle we are looking at
+                if (Mathf.Abs(Mathf.Round(rotation)) == 41) //if looking from left or right
+                {
+                    //newColliderPos = new Vector3(playerPos.x, colliderPos.y, colliderPos.z);
+                    Vector3 Pos = ground.transform.position;
+                    Pos.x = player.transform.position.x;
+                    ground.transform.position = Pos;
+                    prizeCheck(player, true);
+                }
+                else //if looking from front or back
+                {
+                    //newColliderPos = new Vector3(colliderPos.x, colliderPos.y, playerPos.z);
+                    Vector3 Pos = ground.transform.position;
+                    Pos.z = player.transform.position.z;
+                    ground.transform.position = Pos;
+                    prizeCheck(player, true);
+                }
             }
-
-            newColliderPos = collider.transform.InverseTransformPoint(newColliderPos); //local space
-
-            collider.center = newColliderPos; //set in world
         }
     }
 }
---
+++ Fex Clone/Assets/Scripts/RenderOrder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderOrder : MonoBehaviour
+{
+
+    private GameObject obj;
+
+    private void Start()
+    {
+        obj = GameObject.FindGameObjectWithTag("Background");
+        //MeshRenderer rend = obj.GetComponent<MeshRenderer>;
+    }
+}
