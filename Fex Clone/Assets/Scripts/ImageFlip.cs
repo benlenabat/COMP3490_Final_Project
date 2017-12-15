7483b191fe71299c23f455ce2ba21db0d64ad1db
benlenabat <benlenabat@gmail.com>
2017-12-14 19:36:27 -0600

    fresh game from yesterday

---
+++ Fex Clone/Assets/Scripts/ImageFlip.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageFlip : MonoBehaviour {
+
+	// Use this for initialization
+	bool facingLeft = false;
+	private Rigidbody player;
+
+	void Start(){
+		player = gameObject.GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("left") && facingLeft == false){
+			Vector3 theScaleL = transform.localScale;
+			theScaleL.x *= -1;
+			transform.localScale = theScaleL;
+			facingLeft = true;
+		}
+		else if (Input.GetKeyDown("right") && facingLeft == true){
+			Vector3 theScaleR = transform.localScale;
+			theScaleR.x *= -1;
+			transform.localScale = theScaleR;
+			facingLeft = false;
+		}
+	}
+}
